fc90c38c15a463a0d7b18ac85d32f967adc843e8
Niatomi <playervoker@gmail.com>
2022-08-20 21:42:47 +0400

    Add config in startup and init of service

--- src/main/java/ru/niatomi/Application.java
+++ src/main/java/ru/niatomi/Application.java
@@ -1,12 +1,30 @@
 package ru.niatomi;
 
+import ru.niatomi.configuration.CSVResourceFileConfiguration;
+import ru.niatomi.configuration.ColumnChoiceConfiguration;
+import ru.niatomi.service.FindService;
+import ru.niatomi.service.Impl.FindServiceImpl;
+
 /**
  * @author niatomi
  */
 public class Application {
 
-    public static void main(String[] args) {
+    private static final ColumnChoiceConfiguration COLUMN_CHOICE_CONFIGURATION =
+            ColumnChoiceConfiguration.getColumnChoiceConfiguration();
+    private static final CSVResourceFileConfiguration CSV_RESOURCE_FILE_CONFIGURATION =
+            CSVResourceFileConfiguration.getCsvResourceFileConfiguration();
+    private static final String DATASET_NAME = "airports.csv";
 
+    public static void main(String[] args) {
+        configureApp(args);
+        FindService findService = new FindServiceImpl();
+        findService.initService();
+    }
+
+    private static void configureApp(String[] args) {
+        COLUMN_CHOICE_CONFIGURATION.setColumnNumberAndSetPropertyName(args);
+        CSV_RESOURCE_FILE_CONFIGURATION.setFileName(DATASET_NAME);
     }
 
 }
